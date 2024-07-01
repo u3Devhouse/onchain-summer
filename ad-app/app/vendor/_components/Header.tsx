411d1047cdf42ae247cca34a388f9c7dd5709db1
Semi Invader <41244007+jmanywhere@users.noreply.github.com>
2024-06-30 20:50:35 -0600

    change name to CollectAds and collect deploy script

--- ad-app/app/vendor/_components/Header.tsx
+++ ad-app/app/vendor/_components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="flex flex-row items-center justify-center border-b-2 border-black sticky top-0 bg-white/50 backdrop-blur-sm">
       <header className="flex flex-row items-center justify-between px-4 py-2 w-full max-w-[1440px]">
         <div className="flex flex-row items-center justify-center gap-4">
-          <div className="text-3xl font-anton">AdCollector</div>
+          <div className="text-3xl font-anton">CollectAds</div>
           <nav className="md:flex flex-row items-center justify-center gap-2 text-base hidden ">
             <Link className="hover:underline" href="/">
               Home
--- ad-licensing/script/AdLicensingDeploy.s.sol
+++ ad-licensing/script/AdLicensingDeploy.s.sol
@@ -2,7 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {AdLicensing} from "../src/AdLicensing.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+    function setUp() public {
+        address usdc;
+        if (block.chainid == 8453)
+            usdc = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+        else if (block.chainid == 84532)
+            usdc = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+        AdLicensing adLicensing = new AdLicensing(usdc);
+        console.log("AdLicensing deployed at", address(adLicensing));
+    }
 }
