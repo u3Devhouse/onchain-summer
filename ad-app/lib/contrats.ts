5d6603f68343779796856bfab2a824c33443a716
Semi Invader <41244007+jmanywhere@users.noreply.github.com>
2024-07-02 15:40:24 -0600

    redeploy

--- ad-app/lib/contrats.ts
+++ ad-app/lib/contrats.ts
@@ -1,2 +1,2 @@
-export const adLicensingContract = "0xff500480aea2e19ed26fe851f5f4b198558ea7cb";
+export const adLicensingContract = "0x3fADD6A5d1A8533F6082C7B5Af86Aa11CdC0AE44";
 export const usdc = "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
--- ad-licensing/script/AdLicensingDeploy.s.sol
+++ ad-licensing/script/AdLicensingDeploy.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {AdLicensing} from "../src/AdLicensing.sol";
+import {AdLicensing, IAdLicense} from "../src/AdLicensing.sol";
 
 contract CounterScript is Script {
     function run() public {
@@ -13,7 +13,35 @@ contract CounterScript is Script {
             usdc = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
         vm.startBroadcast();
         AdLicensing adLicensing = new AdLicensing(usdc);
-        vm.stopBroadcast();
         console.log("AdLicensing deployed at", address(adLicensing));
+        adLicensing.createAd(
+            0xf11176495d7370DB4d634eb1827dadDB919F62aA,
+            bytes32(uint256(1)),
+            1_000000,
+            30 days,
+            IAdLicense.AdTerms.SINGLE_PAYMENT_DURATION
+        );
+        adLicensing.createAd(
+            0xf11176495d7370DB4d634eb1827dadDB919F62aA,
+            bytes32(uint256(2)),
+            1_000000,
+            30 days,
+            IAdLicense.AdTerms.SINGLE_PAYMENT_DURATION
+        );
+        adLicensing.createAd(
+            0xf11176495d7370DB4d634eb1827dadDB919F62aA,
+            bytes32(uint256(3)),
+            10_000,
+            30 days,
+            IAdLicense.AdTerms.REVSHARE
+        );
+        adLicensing.createAd(
+            0xf11176495d7370DB4d634eb1827dadDB919F62aA,
+            bytes32(uint256(4)),
+            5_000,
+            30 days,
+            IAdLicense.AdTerms.REVSHARE
+        );
+        vm.stopBroadcast();
     }
 }
